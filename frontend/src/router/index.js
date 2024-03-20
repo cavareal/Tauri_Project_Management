7e4b7ea9e55b530edbdb511c504e4463b880194d
CarreauTh <tho.carreau@gmail.com>
2024-03-20 09:48:48 +0100

    feat : adding the dashboard and connnection pages to the router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router"
-import HomeView from "../views/HomeView.vue"
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
 			path: "/",
-			name: "home",
-			component: HomeView
+			name: "dashboard",
+			component: () => import("../views/DashboardView.vue")
 		},
 		{
 			path: "/about",
@@ -16,6 +15,11 @@ const router = createRouter({
 			// this generates a separate chunk (About.[hash].js) for this route
 			// which is lazy-loaded when the route is visited.
 			component: () => import("../views/AboutView.vue")
+		},
+		{
+			path: "/connection",
+			name: "connection",
+			component: () => import("../views/ConnectionView.vue")
 		}
 	]
 })
