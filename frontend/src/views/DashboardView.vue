e58a4ac15dfc0820ca8aeffb53f6b2471620e89b
Quentin TANGUY <quentin.tanguy@reseau.eseo.fr>
2024-03-22 08:07:34 +0100

    feat: add an scroll area in the dashboard

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -4,13 +4,18 @@ import {
 	ResizablePanel,
 	ResizablePanelGroup
 } from "@/components/ui/resizable"
+import { ScrollArea } from "@/components/ui/scroll-area/index.js"
 </script>
 
 <template>
 	<ResizablePanelGroup direction="horizontal">
 		<ResizablePanel class="bg-gray-200 flex items-center justify-center min-w-56 max-w-96">Sidebar</ResizablePanel>
 		<ResizableHandle />
-		<ResizablePanel class="flex items-center justify-center">Content</ResizablePanel>
+		<ResizablePanel class="flex items-center justify-center">
+      <ScrollArea class="h-[100%] w-[100%] rounded-md border p-4">
+        <ScrollBar/>
+      </ScrollArea>
+    </ResizablePanel>
 	</ResizablePanelGroup>
 </template>
 
