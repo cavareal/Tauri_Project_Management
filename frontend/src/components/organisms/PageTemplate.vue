61e17f30c741b19c2d1af9e9a4de5888efdcad4b
Maxence Bonamy <maxencebonamy@gmail.com>
2024-03-25 08:31:02 +0100

    feat: page template

---
+++ frontend/src/components/organisms/PageTemplate.vue
@@ -0,0 +1,29 @@
+<script>
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable/index.js"
+import Sidebar from "@/components/organisms/Sidebar.vue"
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area/index.js"
+
+export default {
+	name: "PageTemplate",
+	components: { ResizablePanelGroup, ResizableHandle, ScrollArea, ScrollBar, Sidebar, ResizablePanel }
+}
+</script>
+
+<template>
+	<ResizablePanelGroup direction="horizontal">
+		<ResizablePanel class="" :default-size="16" :min-size="16" :max-size="24">
+			<Sidebar />
+		</ResizablePanel>
+		<ResizableHandle />
+		<ResizablePanel class="flex items-center justify-center">
+			<ScrollArea class="h-[100%] w-[100%] rounded-md border p-4">
+				<slot/>
+				<ScrollBar/>
+			</ScrollArea>
+		</ResizablePanel>
+	</ResizablePanelGroup>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,25 +1,11 @@
 <script setup>
-import {
-	ResizableHandle,
-	ResizablePanel,
-	ResizablePanelGroup
-} from "@/components/ui/resizable"
-import Sidebar from "@/components/organisms/Sidebar.vue"
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
+import PageTemplate from "@/components/organisms/PageTemplate.vue"
 </script>
 
 <template>
-	<ResizablePanelGroup direction="horizontal">
-		<ResizablePanel class="" :default-size="16" :min-size="16" :max-size="24">
-			<Sidebar />
-		</ResizablePanel>
-		<ResizableHandle />
-		<ResizablePanel class="flex items-center justify-center">
-			<ScrollArea class="h-[100%] w-[100%] rounded-md border p-4">
-				<ScrollBar/>
-			</ScrollArea>
-		</ResizablePanel>
-	</ResizablePanelGroup>
+	<PageTemplate>
+		Hello World
+	</PageTemplate>
 </template>
 
 <style scoped>
