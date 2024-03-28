a28c10c95e34ca236307efa036fd8e2d54979336
charl56 <charles.pichou56@gmail.com>
2024-03-28 17:57:25 +0100

    Feat : teams menu create/ Pr10, pr11

--- frontend/src/components/pages/TeamsView.vue
+++ frontend/src/components/pages/TeamsView.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import PageTemplate from "@/components/organisms/PageTemplate.vue"
+import TeamsNotsCreated from "@/components/organisms/Teams/TeamsNotsCreated.vue"
+import StudentsNotImported from "@/components/organisms/Teams/StudentsNotImported.vue"
+
 </script>
 
 <template>
 	<PageTemplate>
-		Teams
+		<h1 class="text-3xl font-title-bold">Equipes</h1>
+		<StudentsNotImported />
+		<TeamsNotsCreated />
 	</PageTemplate>
 </template>
