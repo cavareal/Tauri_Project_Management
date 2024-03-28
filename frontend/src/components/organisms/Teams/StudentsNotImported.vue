497666eb751c367bddb951760725c0ad462451d8
charl56 <charles.pichou56@gmail.com>
2024-03-28 19:04:17 +0100

    feat requets with params to create teams

---
+++ frontend/src/components/organisms/Teams/StudentsNotImported.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button"
+import { ExternalLink } from 'lucide-vue-next';
+</script>
+
+<template>
+    <div class="border border-gray-300 rounded-lg p-2 md:p-3 my-4">
+        <h1 class="font-bold text-base">Vous n'avez pas encore importé les étudiants</h1>
+        <p class="text-gray-400 text-sm">Pour générer les équipes, il faut commencer par importer les étudiants.</p>
+        <div class="w-full pt-5 pb-2 px-0 flex justify-center">
+            <a href="students">
+                <Button variant="destructive">
+                    <ExternalLink class="w-5 h-5" />
+                    <span class="mx-5 my-0">Importer les étudiants</span>
+                </Button>
+            </a>
+        </div>
+
+
+    </div>
+</template>
