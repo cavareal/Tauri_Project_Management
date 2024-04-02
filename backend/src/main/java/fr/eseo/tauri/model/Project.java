b626fe5d515a78bf87f10367e7efc78352691804
Théo Marques <theo.marques@reseau.eseo.fr>
2024-04-02 09:21:20 +0200

    refactor:  change of name

--- backend/src/main/java/fr/eseo/tauri/model/Project.java
+++ backend/src/main/java/fr/eseo/tauri/model/Project.java
@@ -29,7 +29,7 @@ public class Project {
     private Integer nbSprint;
 
     @Enumerated(EnumType.STRING)
-    @Column(name="project_phase")
+    @Column(name="phase")
     @JsonProperty
     private ProjectPhase phase;
 
