29f96f192ce9e1e1d1dabb0679c1a14609cd8432
Théo Marques <theo.marques@reseau.eseo.fr>
2024-04-04 11:47:41 +0200

    feat : addition of 1 missing repository

---
+++ backend/src/main/java/fr/eseo/tauri/repository/ScaleTypesRepository.java
@@ -0,0 +1,7 @@
+package fr.eseo.tauri.repository;
+
+import fr.eseo.tauri.model.ScaleTypes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScaleTypesRepository extends JpaRepository<ScaleTypes, Integer> {
+}
