418e813970970f575a57adf83cca4a51b2ec31e3
Quentin TANGUY <quentin.tanguy@reseau.eseo.fr>
2024-04-07 14:59:57 +0200

    fix : issues config

--- frontend/src/components/molecules/tab/Tab.vue
+++ frontend/src/components/molecules/tab/Tab.vue
@@ -1,9 +1,9 @@
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from "vue"
 export default defineComponent({
-  name: "Tab",
-  props: ["title", "titleSlot", "disabled"],
-});
+	name: "Tab",
+	props: ["title", "titleSlot", "disabled"]
+})
 </script>
 
 <template>
