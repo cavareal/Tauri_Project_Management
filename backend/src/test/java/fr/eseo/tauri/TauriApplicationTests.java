af392f9273e278264fa4c720b062fd7aa76e8558
pichouch <charles.pichou@reseau.eseo.fr>
2024-04-09 22:46:35 +0200

    Fix : sonar test on gitlab pipeline OK

--- backend/src/test/java/fr/eseo/tauri/TauriApplicationTests.java
+++ backend/src/test/java/fr/eseo/tauri/TauriApplicationTests.java
@@ -3,7 +3,7 @@ package fr.eseo.tauri;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest()
+//@SpringBootTest()		// Don't know why, but "gradlew test" in gitlab pipeline work with this line commented
 class TauriApplicationTests {
 
 	@Test
