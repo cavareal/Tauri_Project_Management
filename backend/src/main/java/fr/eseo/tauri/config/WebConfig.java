3f1ecaf4975380bc7975e0f922cdecfeb8669d40
pichouch <charles.pichou@reseau.eseo.fr>
2024-04-10 11:28:18 +0200

    FIX gitlab ci

--- backend/src/main/java/fr/eseo/tauri/config/WebConfig.java
+++ backend/src/main/java/fr/eseo/tauri/config/WebConfig.java
@@ -25,12 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/{spring:\\w+}")
-                .setViewName("forward:/");
-        registry.addViewController("/**/{spring:\\w+}")
-                .setViewName("forward:/");
-        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
-                .setViewName("forward:/");
+        registry.addViewController("/**").setViewName("forward:/");
     }
 
 }
