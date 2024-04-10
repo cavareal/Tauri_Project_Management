3df045917a77406ea8017d651ee8a29ff1b5b358
pichouch <charles.pichou@reseau.eseo.fr>
2024-04-10 11:25:00 +0200

    FIX gitlab ci

--- backend/src/main/java/fr/eseo/tauri/controller/IndexController.java
+++ backend/src/main/java/fr/eseo/tauri/controller/IndexController.java
@@ -13,58 +13,58 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+//
+//@RestController
+//@RequestMapping("/")
+//public class IndexController {
 
-@RestController
-@RequestMapping("/")
-public class IndexController {
-
-    @GetMapping("")
-    public String index() {
-        try {
-            Resource resource = new ClassPathResource("static/index.html");
-            return StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            CustomLogger.logError("An error occurred in get indexController", e);
-            return "Error loading index.html";
-        }
-    }
-
-    @GetMapping("/assets/{fileName}")
-    public ResponseEntity<byte[]> getAsset(@PathVariable String fileName) {
-        try {
-            Resource resource = new ClassPathResource("static/assets/" + fileName);
-            byte[] data = StreamUtils.copyToByteArray(resource.getInputStream());
-
-            // Determine the media type based on the file extension
-            MediaType mediaType = MediaType.APPLICATION_OCTET_STREAM;
-            if (fileName.endsWith(".js")) {
-                mediaType = MediaType.valueOf("application/javascript");
-            } else if (fileName.endsWith(".css")) {
-                mediaType = MediaType.valueOf("text/css");
-            }
-
-            return ResponseEntity.ok()
-                    .contentType(mediaType)
-                    .body(data);
-        } catch (IOException e) {
-            CustomLogger.logError("An error occurred in get asset", e);
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    @GetMapping(value = "/{fileName:.+}")
-    public ResponseEntity<byte[]> getImage(@PathVariable String fileName) throws IOException {
-        MediaType mediaType = MediaType.APPLICATION_OCTET_STREAM;
-        if (fileName.endsWith(".svg")) {
-            mediaType = MediaType.valueOf("image/svg+xml");
-        } else if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg")) {
-            mediaType = MediaType.IMAGE_JPEG;
-        } else {
-            return ResponseEntity.badRequest().build();
-        }
-
-        Resource resource = new ClassPathResource("static/" + fileName);
-        byte[] data = StreamUtils.copyToByteArray(resource.getInputStream());
-        return ResponseEntity.ok().contentType(mediaType).body(data);
-    }
-}
+//    @GetMapping("")
+//    public String index() {
+//        try {
+//            Resource resource = new ClassPathResource("static/index.html");
+//            return StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
+//        } catch (IOException e) {
+//            CustomLogger.logError("An error occurred in get indexController", e);
+//            return "Error loading index.html";
+//        }
+//    }
+//
+//    @GetMapping("/assets/{fileName}")
+//    public ResponseEntity<byte[]> getAsset(@PathVariable String fileName) {
+//        try {
+//            Resource resource = new ClassPathResource("static/assets/" + fileName);
+//            byte[] data = StreamUtils.copyToByteArray(resource.getInputStream());
+//
+//            // Determine the media type based on the file extension
+//            MediaType mediaType = MediaType.APPLICATION_OCTET_STREAM;
+//            if (fileName.endsWith(".js")) {
+//                mediaType = MediaType.valueOf("application/javascript");
+//            } else if (fileName.endsWith(".css")) {
+//                mediaType = MediaType.valueOf("text/css");
+//            }
+//
+//            return ResponseEntity.ok()
+//                    .contentType(mediaType)
+//                    .body(data);
+//        } catch (IOException e) {
+//            CustomLogger.logError("An error occurred in get asset", e);
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
+//
+//    @GetMapping(value = "/{fileName:.+}")
+//    public ResponseEntity<byte[]> getImage(@PathVariable String fileName) throws IOException {
+//        MediaType mediaType = MediaType.APPLICATION_OCTET_STREAM;
+//        if (fileName.endsWith(".svg")) {
+//            mediaType = MediaType.valueOf("image/svg+xml");
+//        } else if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg")) {
+//            mediaType = MediaType.IMAGE_JPEG;
+//        } else {
+//            return ResponseEntity.badRequest().build();
+//        }
+//
+//        Resource resource = new ClassPathResource("static/" + fileName);
+//        byte[] data = StreamUtils.copyToByteArray(resource.getInputStream());
+//        return ResponseEntity.ok().contentType(mediaType).body(data);
+//    }
+//}
