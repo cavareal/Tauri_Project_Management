319348190cf74360b48b7e6e0781457cd7ab26fb
pichouch <charles.pichou@reseau.eseo.fr>
2024-04-12 13:38:45 +0200

    Fix router frontend

--- backend/src/main/java/fr/eseo/tauri/config/WebConfig.java
+++ backend/src/main/java/fr/eseo/tauri/config/WebConfig.java
@@ -3,12 +3,10 @@ package fr.eseo.tauri.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-//@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${cors.allowed.origins}")
@@ -22,11 +20,18 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*");
     }
 
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/**").setViewName("forward:/");
-//        registry.addViewController("/{spring:\\w+}/**").setViewName("forward:/");
-//    }
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Map "/"
+        registry.addViewController("/")
+                .setViewName("forward:/index.html");
 
+        // Single directory level - no need to exclude "api"
+        registry.addViewController("/{x:[\\w\\-]+}")
+                .setViewName("forward:/index.html");
+        // Multi-level directory path, need to exclude "api" on the first part of the path
+        registry.addViewController("/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}")
+                .setViewName("forward:/index.html");
+    }
 
 }
