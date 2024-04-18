2448c4448bc5a92b5389d8c5ce5c81fa8279a3fd
pichouch <charles.pichou@reseau.eseo.fr>
2024-04-18 08:32:16 +0200

    CI gitlab and env var for DB

--- backend/src/main/java/fr/eseo/tauri/TauriApplication.java
+++ backend/src/main/java/fr/eseo/tauri/TauriApplication.java
@@ -3,12 +3,19 @@ package fr.eseo.tauri;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "fr.eseo.tauri")
 public class TauriApplication {
 
 	public static void main(String[] args) {
+		// Load database logs
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("spring.datasource.username", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DATABASE_PASSWORD"));
+		// Run the application
 		SpringApplication.run(TauriApplication.class, args);
 	}
 
