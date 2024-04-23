fd06b0f0a9147899617e35015f14a8225f553283
pichouch <charles.pichou@reseau.eseo.fr>
2024-04-23 16:35:50 +0200

    CI gitlab ci

--- backend/src/main/java/fr/eseo/tauri/TauriApplication.java
+++ backend/src/main/java/fr/eseo/tauri/TauriApplication.java
@@ -12,17 +12,23 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class TauriApplication {
 
 	public static void main(String[] args) {
-		// Load database logs
-		try{
-//			Dotenv dotenv = Dotenv.load();
-//			System.setProperty("spring.datasource.username", dotenv.get("DATABASE_USERNAME"));
-//			System.setProperty("spring.datasource.password", dotenv.get("DATABASE_PASSWORD"));
-//			System.out.println(System.getenv("DATABASE_USERNAME"));
-			System.setProperty("spring.datasource.username", System.getProperty("databaseUsername"));
-			System.setProperty("spring.datasource.password", System.getProperty("databasePassword"));
-		} catch (Exception e){
-			CustomLogger.logError("No .env file found, using default values", e);
+		String databaseUsername = System.getProperty("databaseUsername");
+		String databasePassword = System.getProperty("databasePassword");
+
+		if (databaseUsername != null && databasePassword != null) {
+			System.setProperty("spring.datasource.username", databaseUsername);
+			System.setProperty("spring.datasource.password", databasePassword);
+		} else {
+			// Fallback to .env file for dev environment
+			try {
+				Dotenv dotenv = Dotenv.load();
+				System.setProperty("spring.datasource.username", dotenv.get("DATABASE_USERNAME"));
+				System.setProperty("spring.datasource.password", dotenv.get("DATABASE_PASSWORD"));
+			} catch (Exception e) {
+				CustomLogger.logError("No .env file found, using default values", e);
+			}
 		}
+
 		// Run the application
 		SpringApplication.run(TauriApplication.class, args);
 	}
