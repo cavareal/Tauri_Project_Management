fa4ce7d9c90754f6340a471d0ae942321feb3710
pichouch <charles.pichou@reseau.eseo.fr>
2024-04-24 08:40:35 +0200

    CI gitlab ci

--- backend/src/main/java/fr/eseo/tauri/TauriApplication.java
+++ backend/src/main/java/fr/eseo/tauri/TauriApplication.java
@@ -15,8 +15,19 @@ public class TauriApplication {
 		// Load database logs
 		try{
 			Dotenv dotenv = Dotenv.load();
-			System.setProperty("spring.datasource.username", dotenv.get("DATABASE_USERNAME"));
-			System.setProperty("spring.datasource.password", dotenv.get("DATABASE_PASSWORD"));
+			String databaseUsername = "DATABASE_USERNAME";
+			String databasePassword = "DATABASE_PASSWORD";
+
+			CustomLogger.logInfo(dotenv.get(databaseUsername));
+			CustomLogger.logInfo(dotenv.get(databasePassword));
+
+			if(dotenv.get(databaseUsername) != null && !dotenv.get(databaseUsername).isEmpty()) {
+				System.setProperty("spring.datasource.username", dotenv.get(databaseUsername));
+			}
+			if(dotenv.get(databasePassword) != null && !dotenv.get(databasePassword).isEmpty()) {
+				System.setProperty("spring.datasource.password", dotenv.get(databasePassword));
+			}
+
 		} catch (Exception e){
 			CustomLogger.logError("No .env file found, using default values", e);
 		}
