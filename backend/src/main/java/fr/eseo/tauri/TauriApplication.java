66c42d1f797c004169c4790d92b8468f67add7c8
pichouch <charles.pichou@reseau.eseo.fr>
2024-04-24 08:52:04 +0200

    CI gitlab ci

--- backend/src/main/java/fr/eseo/tauri/TauriApplication.java
+++ backend/src/main/java/fr/eseo/tauri/TauriApplication.java
@@ -4,6 +4,7 @@ import fr.eseo.tauri.util.CustomLogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.beans.factory.annotation.Value;
 import io.github.cdimascio.dotenv.Dotenv;
 
 
@@ -11,26 +12,40 @@ import io.github.cdimascio.dotenv.Dotenv;
 @EnableJpaRepositories(basePackages = "fr.eseo.tauri")
 public class TauriApplication {
 
+	@Value("${prod}")
+	private static boolean prod;
+	@Value("${DATABASE_USERNAME}")
+	private static String databaseUsernameProd;
+	@Value("${DATABASE_PASSWORD}")
+	private static String databasePasswordProd;
+
+
 	public static void main(String[] args) {
-		// Load database logs
-		try{
-			Dotenv dotenv = Dotenv.load();
-			String databaseUsername = "DATABASE_USERNAME";
-			String databasePassword = "DATABASE_PASSWORD";
 
-			CustomLogger.logInfo(dotenv.get(databaseUsername));
-			CustomLogger.logInfo(dotenv.get(databasePassword));
+		if(prod){		// If build for production deploiement
+			System.setProperty("spring.datasource.username", databaseUsernameProd);
+			System.setProperty("spring.datasource.password", databasePasswordProd);
+		} else {
+			try{
+				Dotenv dotenv = Dotenv.load();
+				String databaseUsername = "DATABASE_USERNAME";
+				String databasePassword = "DATABASE_PASSWORD";
 
-			if(dotenv.get(databaseUsername) != null && !dotenv.get(databaseUsername).isEmpty()) {
-				System.setProperty("spring.datasource.username", dotenv.get(databaseUsername));
+				CustomLogger.logInfo(dotenv.get(databaseUsername));
+				CustomLogger.logInfo(dotenv.get(databasePassword));
+
+				if(dotenv.get(databaseUsername) != null && !dotenv.get(databaseUsername).isEmpty()) {
+					System.setProperty("spring.datasource.username", dotenv.get(databaseUsername));
+				}
+				if(dotenv.get(databasePassword) != null && !dotenv.get(databasePassword).isEmpty()) {
+					System.setProperty("spring.datasource.password", dotenv.get(databasePassword));
+				}
+
+			} catch (Exception e){
+				CustomLogger.logError("No .env file found, using default values", e);
 			}
-			if(dotenv.get(databasePassword) != null && !dotenv.get(databasePassword).isEmpty()) {
-				System.setProperty("spring.datasource.password", dotenv.get(databasePassword));
-			}
-
-		} catch (Exception e){
-			CustomLogger.logError("No .env file found, using default values", e);
 		}
+
 		// Run the application
 		SpringApplication.run(TauriApplication.class, args);
 	}
