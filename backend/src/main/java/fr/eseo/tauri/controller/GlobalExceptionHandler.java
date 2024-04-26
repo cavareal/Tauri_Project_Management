2313925e6c5ef6e6b6b50a2f208e575ef9fb0e79
Théo Marques <theo.marques@reseau.eseo.fr>
2024-04-26 15:18:22 +0200

    refactor : The message returned by GlobalExceptionHandler is now a JSON object

--- backend/src/main/java/fr/eseo/tauri/controller/GlobalExceptionHandler.java
+++ backend/src/main/java/fr/eseo/tauri/controller/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package fr.eseo.tauri.controller;
 
+import fr.eseo.tauri.model.exception.ExceptionResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpStatus;
@@ -10,40 +11,28 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.method.HandlerMethod;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
-    /**
-     * Method to generate the message containing all the necessary information to localize the origin of the exception, its type, and give clues on how to fix it.
-     * @param e the exception
-     * @param handlerMethod the method that threw the exception
-     * @param request the request that caused the exception
-     * @return the error message
-     */
-    private String generateMessage(Exception e, HandlerMethod handlerMethod, HttpServletRequest request) {
-        return "An exception has been thrown at " + handlerMethod.getMethod().getDeclaringClass() + ", by " + handlerMethod.getMethod().getName() + " method."  + " \n\nType of exception : " +e.getClass().getCanonicalName() + "\n\nMessage : " + e.getMessage() + "\n\nRequest path : " + request.getServletPath();
-    }
 
     //First we handle all the exceptions related to bad parameters for the requests
     //Pour les exceptions de validation, utiliser BAD REQUEST
     @ExceptionHandler(value = {IllegalArgumentException.class, NumberFormatException.class, ArrayIndexOutOfBoundsException.class,
             ServletRequestBindingException.class, HttpMessageNotReadableException.class, TypeMismatchException.class,
             HandlerMethodValidationException.class, MethodArgumentNotValidException.class})
-    public ResponseEntity<Object> handleBadRequests(Exception e, HandlerMethod handlerMethod, HttpServletRequest request) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(generateMessage(e, handlerMethod, request));
+    public ResponseEntity<ExceptionResponse> handleBadRequests(Exception e, HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionResponse(e,  request));
     }
 
     //Handle the exception related tou not found elements
     @ExceptionHandler(value = NullPointerException.class)
-    public ResponseEntity<Object> handleNullPointerException(NullPointerException e, HandlerMethod handlerMethod, HttpServletRequest request) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(generateMessage(e, handlerMethod, request));
+    public ResponseEntity<ExceptionResponse> handleNullPointerException(NullPointerException e, HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionResponse(e, request));
     }
 
     //Handle all other exceptions
     @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<Object> handleUnhandledExceptions(Exception e, HandlerMethod handlerMethod, HttpServletRequest request) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(generateMessage(e, handlerMethod, request));
+    public ResponseEntity<ExceptionResponse> handleUnhandledExceptions(Exception e, HttpServletRequest request) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ExceptionResponse(e, request));
     }
 }
---
+++ backend/src/main/java/fr/eseo/tauri/model/exception/ExceptionResponse.java
@@ -0,0 +1,24 @@
+package fr.eseo.tauri.model.exception;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExceptionResponse {
+
+    @JsonProperty
+    private String requestPath;
+
+    @JsonProperty
+    private String message;
+
+    public ExceptionResponse(Exception exception,  HttpServletRequest request) {
+        this.requestPath = request.getServletPath();
+        this.message = exception.getMessage();
+
+    }
+}
