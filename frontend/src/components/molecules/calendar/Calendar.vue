f5d68966c68b5944f41572644239b7b4e68eacae
pichouch <charles.pichou@reseau.eseo.fr>
2024-05-02 15:21:47 +0200

    Feat : add calendar for sprint type and duration

--- frontend/src/components/molecules/calendar/Calendar.vue
+++ frontend/src/components/molecules/calendar/Calendar.vue
@@ -1,75 +1,67 @@
 <script setup lang="ts">
 import { computed, h, ref } from 'vue'
 import { CalendarDate, DateFormatter, getLocalTimeZone, parseDate, today } from '@internationalized/date'
+import { toDate } from 'radix-vue'
+import { CalendarCheck } from "lucide-vue-next"
+import { z } from 'zod'
 import { Calendar } from '@/components/ui/calendar'
 import { Button } from '@/components/ui/button'
-import {
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from '@/components/ui/form'
+import { useForm } from 'vee-validate' 
+
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 
 const df = new DateFormatter('en-US', {
     dateStyle: 'long',
 })
+const props = defineProps({
+ minValue: {
+    type: CalendarDate, // Assuming CalendarDate or similar
+    default: () => null,
+ },
+})
 
-
+const formSchema = z.object({
+    dob: z
+        .string()
+        .refine(v => v, { message: 'Une date est requise !' }),
+})
 
 const placeholder = ref()
 
-const { handleSubmit, setValues, values } = null
+const { setValues, values } = useForm({
+    validationSchema: formSchema,
+})
 
 const value = computed({
     get: () => values.dob ? parseDate(values.dob) : undefined,
     set: val => val,
 })
 
-const onSubmit = handleSubmit(() => {
-    console.log(values)
-    console.log(value)
-})
 </script>
 
 <template>
-    <form class="space-y-8" @submit="onSubmit">
-        <FormField name="dob">
-            <FormItem class="flex flex-col">
-                <FormLabel>Date of birth</FormLabel>
-                <Popover>
-                    <PopoverTrigger as-child>
-                        <FormControl>
-                            <Button variant="outline">
-                                <span>Pick a date</span>
-                            </Button>
-                            <input hidden>
-                        </FormControl>
-                    </PopoverTrigger>
-                    <PopoverContent class="w-auto p-0">
-                        <Calendar v-model:placeholder="placeholder" v-model="value" calendar-label="Date of birth"
-                            initial-focus :min-value="new CalendarDate(1900, 1, 1)" @update:model-value="(v) => {
-                                console.log(v)
-                                if (v) {
-                                    setValues({
-                                        dob: v.toString(),
-                                    })
-                                }
-                                else {
-                                    setValues({
-                                        dob: '',
-                                    })
-                                }
-                            }" />
-                    </PopoverContent>
-                </Popover>
-                <FormMessage />
-            </FormItem>
-        </FormField>
-        <Button type="submit">
-            Submit
-        </Button>
-    </Form>
+    <Popover>
+        <PopoverTrigger as-child>
+            <Button variant="outline" class="w-[240px] ps-3 text-start font-normal">
+                <span>{{ value ? df.format(toDate(value)) : "Pick a date" }}</span>
+                <CalendarCheck class="ms-auto h-4 w-4 opacity-50" />
+            </Button>
+            <input hidden>
+        </PopoverTrigger>
+        <PopoverContent class="w-auto p-0">
+            <Calendar v-model:placeholder="placeholder" v-model="value" calendar-label="Date of birth" initial-focus
+                :min-value="props.minValue" :max-value="today(getLocalTimeZone())" @update:model-value="(v) => {
+                    if (v) {
+                        setValues({
+                            dob: v.toString(),
+                        })
+                    }
+                    else {
+                        setValues({
+                            dob: '',
+                        })
+                    }
+                }" />
+        </PopoverContent>
+    </Popover>
 </template>
--- frontend/src/components/organisms/sprints/AddSprintDialog.vue
+++ frontend/src/components/organisms/sprints/AddSprintDialog.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
 
-import { ref } from "vue"
+import { ref, watch } from "vue"
 import LoadingButton from "@/components/molecules/buttons/LoadingButton.vue"
 import UploadArea from "@/components/molecules/upload-area/UploadArea.vue"
 import ErrorText from "@/components/atoms/texts/ErrorText.vue"
 import { importStudentFile } from "@/services/student-service"
 import { CustomDialog, DialogClose } from "@/components/molecules/dialog"
 import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/molecules/calendar/Calendar.vue"
-
+import Calendar from "@/components/molecules/calendar/Calendar.vue"
+import { Column, Row } from "@/components/atoms/containers"
+import {
+  Select,
+} from '@/components/ui/select'
 import { useMutation } from "@tanstack/vue-query"
+import { CalendarDate } from '@internationalized/date'
 
 const DIALOG_TITLE = "Ajouter un sprint"
 const DIALOG_DESCRIPTION = "Pour ajouter un sprint, vous devez spécifier les dates de début et de fin, ainsi que le type de sprint."
@@ -18,14 +22,11 @@ const open = ref(false)
 
 const emits = defineEmits(["add:sprint"])
 
-const file = ref<File | null>(null)
+const startDate = ref<CalendarDate | null>(null)
+const endDate = ref<CalendarDate | null>(null)
 
-const { error, isPending, mutate: upload } = useMutation({ mutationKey: ["add-sprint"], mutationFn: async() => {
-	if (!file.value) return
-	await importStudentFile(file.value)
-		.then(() => open.value = false)
-		.then(() => emits("add:sprint"))
-} })
+
+
 
 </script>
 
@@ -36,10 +37,23 @@ const { error, isPending, mutate: upload } = useMutation({ mutationKey: ["add-sp
 		</template>
 
 		<div>
-			<Calendar />
+			<Row class="flex items-center my-2">
+				<Label class="w-1/2">Date de début</Label>
+				<Calendar v-model="startDate" />
+			</Row>
+			<Row class="flex items-center my-2">
+				<Label class="w-1/2">Date de fin</Label>
+				<Calendar v-model="endDate" :min-value="startDate" />
+			</Row>
+			<Row class="flex items-center my-2">
+				<Label class="w-1/2">Type de fin de sprint</Label>
+				<Select></Select>
+			</Row>
 		</div>
-		
+
+	<!--
 		<ErrorText v-if="error" class="mb-2">Une erreur est survenue lors de l'ajout du sprint.</ErrorText>
+	-->	
 
 		<template #footer>
 			<DialogClose>
