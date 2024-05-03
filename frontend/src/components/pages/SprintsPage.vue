aaf547d2c3f8c2a27df58b82ef1107938bb37c13
pichouch <charles.pichou@reseau.eseo.fr>
2024-05-03 16:30:18 +0200

    FEAT : add request between front and back

--- frontend/src/components/pages/SprintsPage.vue
+++ frontend/src/components/pages/SprintsPage.vue
@@ -4,9 +4,11 @@ import { SidebarTemplate } from "@/components/templates"
 import { getCookie } from "@/utils/cookie"
 import EditNbSprints from "@/components/organisms/sprints/EditNbSprints.vue"
 import AddSprint from "@/components/organisms/sprints/AddSprint.vue"
-import NotAuthorized from "@/components/organisms/errors/NotAuthorized.vue"
+import { Error, NotAuthorized } from "@/components/organisms/errors"
 import type { RoleType } from "@/types/role"
 import { Header } from "@/components/molecules/header"
+import { useQuery } from "@tanstack/vue-query"
+import { getAllSprints } from "@/services/sprint-service"
 
 const token = getCookie("token")
 const role = getCookie<RoleType>("role")
@@ -18,11 +20,18 @@ const role = getCookie<RoleType>("role")
 // TODO
 // Add and edit : same or 2 different components ??
 
+const { data: sprints, error: error } = useQuery({
+	queryKey: ["sprints"], queryFn: () => getAllSprints()
+})
+
+
 </script>
 
 <template>
 	<SidebarTemplate>
 		<Header title="Sprints" />
+		<p>{{ sprints }}</p>
+		<Error v-if="error" />
 		<EditNbSprints v-if="token && (role === 'PROJECT_LEADER' || role === 'OPTION_LEADER')" />
 		<AddSprint v-if="token && (role === 'PROJECT_LEADER' || role === 'OPTION_LEADER')" />
 		<NotAuthorized v-else />
--- frontend/src/services/sprint-service.ts
+++ frontend/src/services/sprint-service.ts
@@ -1,12 +1,31 @@
+import type { Sprint } from "@/types/sprint"
+import { SprintSchema } from "@/types/sprint"
 import { mutateAndValidate, queryAndValidate } from "@/utils/api"
 import { z } from "zod"
-import { SprintSchema } from "@/types/sprint"
+import { getCookie } from "@/utils/cookie"
 
 
+export const getAllSprints = async (): Promise<Sprint[]> => {
 
-export const addSprint = async(sprint: unknown): Promise<void> => {
-	
-    const response = await mutateAndValidate({
+	const currentProjectId = getCookie("currentProject")
+	console.log("getAllSprints", currentProjectId)
+
+	const response = await queryAndValidate({
+		responseSchema: SprintSchema.array(),
+		params: { projectId: "1" },
+		route: "sprints"
+	})
+
+	if (response.status === "error") {
+		throw new Error(response.error)
+	}
+
+	return response.data
+}
+
+export const addSprint = async (sprint: unknown): Promise<void> => {
+
+	const response = await mutateAndValidate({
 		method: "POST",
 		route: `sprints/`,
 		body: sprint,
