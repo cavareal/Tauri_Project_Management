aa68034f0af81a75c64202f3492058cdd7ebd4a2
Théo Marques <theo.marques@reseau.eseo.fr>
2024-05-05 23:49:25 +0200

    fix : addition of display names for GradeTypes

--- backend/src/main/java/fr/eseo/tauri/model/enumeration/GradeTypeName.java
+++ backend/src/main/java/fr/eseo/tauri/model/enumeration/GradeTypeName.java
@@ -7,13 +7,15 @@ import lombok.Getter;
 @Getter
 public enum GradeTypeName {
 
-    AVERAGE,
-    TECHNICAL_SOLUTION,
-    SPRINT_CONFORMITY,
-    PROJECT_MANAGEMENT,
-    PRESENTATION_CONTENT,
-    PRESENTATION_SUPPORT_MATERIAL,
-    TEAM_PERFORMANCE,
-    INDIVIDUAL_PERFORMANCE;
+    AVERAGE("Moyenne"),
+    TECHNICAL_SOLUTION("Solution Technique"),
+    SPRINT_CONFORMITY("Conformité du sprint"),
+    PROJECT_MANAGEMENT("Gestion de projet"),
+    PRESENTATION_CONTENT("Contenu de la présentation"),
+    PRESENTATION_SUPPORT_MATERIAL("Support de présentation"),
+    TEAM_PERFORMANCE("Performance de l'équipe"),
+    INDIVIDUAL_PERFORMANCE("Performance individuelle");
+
+    private final String displayName;
 
 }
