d4737f75e07220fd244f8bdc640c70ee02c50c3c
CarreauTh <tho.carreau@gmail.com>
2024-05-12 23:55:22 +0200

    Feat : adding annotations (quality changes)

--- backend/src/main/java/fr/eseo/tauri/security/JwtTokenFilter.java
+++ backend/src/main/java/fr/eseo/tauri/security/JwtTokenFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -17,7 +18,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
     private final JwtTokenUtil jwtTokenUtil;
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
         String token = jwtTokenUtil.extractToken(request);
 
         if (token != null && jwtTokenUtil.validateAccessToken(token)) {
