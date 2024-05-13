2f721914a25a1bec9456cfede796279a2a164176
pichouch <charles.pichou@reseau.eseo.fr>
2024-05-13 11:33:59 +0200

    FIX package.json duplications

--- backend/src/main/java/fr/eseo/tauri/TauriApplication.java
+++ backend/src/main/java/fr/eseo/tauri/TauriApplication.java
@@ -9,13 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "fr.eseo.tauri")
 public class TauriApplication {
 
-	@Value("${prod}")
-	private static boolean prod;
-	@Value("${databaseUsername}")
-	private static String databaseUsernameProd;
-	@Value("${databasePassword}")
-	private static String databasePasswordProd;
-
 	public static void main(String[] args) {
 		// Run the application
 		SpringApplication.run(TauriApplication.class, args);
