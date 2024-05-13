c3c44a6c6641db745774be3e708cb59137a23efd
Maxence Bonamy <maxencebonamy@gmail.com>
2024-05-13 23:08:19 +0200

    feat: change style of login page

--- frontend/src/components/molecules/connection/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as ConnectionForm } from "./ConnectionForm.vue"
--- frontend/src/components/pages/ConnectionPage.vue
+++ frontend/src/components/pages/ConnectionPage.vue
@@ -1,22 +1,33 @@
 <script setup lang="ts">
 
 import { Logo } from "@/components/atoms/logo"
-import { Column } from "@/components/atoms/containers"
+import { Column, Row } from "@/components/atoms/containers"
 import Title from "@/components/atoms/texts/Title.vue"
 import { Cookies } from "@/utils/cookie"
-import { ConnectionForm } from "@/components/molecules/connection"
+import { ConnectionForm } from "@/components/organisms/connection"
 
 Cookies.removeAll()
 
 </script>
 
 <template>
-	<Column class="bg-cover bg-white bg-center size-full items-center justify-start gap-12 pt-12 bg-eseo">
+	<!-- <Column class="bg-cover bg-white bg-center size-full items-center justify-start gap-12 pt-12 bg-eseo">
 		<Column class="items-center gap-4">
 			<Logo class="h-32 fill-dark-blue" />
 			<Title class="text-dark-blue">Bienvenue sur Tauri !</Title>
 		</Column>
 
 		<ConnectionForm />
-	</Column>
+	</Column> -->
+	<Row class="w-full h-full items-stretch justify-stretch">
+		<img src="/background.png" alt="Background with Tauri Logo" class="w-1/2 h-full object-cover object-top" />
+
+		<Column class="w-1/2 h-full items-center justify-center gap-4 bg-white">
+			<Logo class="h-48 fill-dark-blue" />
+
+			<Title class="text-dark-blue text-4xl">Bienvenue sur Tauri !</Title>
+
+			<ConnectionForm />
+		</Column>
+	</Row>
 </template>
