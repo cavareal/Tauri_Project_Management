6166f32fe47f80ac8a6230d85bcabf9ba7349812
corentinrobin <corentin.robin@reseau.eseo.fr>
2024-05-15 18:57:16 +0200

    Test: Add JUnit test to AuthServiceTest.java

--- backend/src/test/java/fr/eseo/tauri/service/AuthServiceTest.java
+++ backend/src/test/java/fr/eseo/tauri/service/AuthServiceTest.java
@@ -1,13 +1,18 @@
 package fr.eseo.tauri.service;
 
-import fr.eseo.tauri.util.CustomLogger;
+import fr.eseo.tauri.model.User;
+import fr.eseo.tauri.security.AuthResponse;
+import fr.eseo.tauri.security.JwtTokenUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class AuthServiceTest {
 
@@ -15,7 +20,13 @@ class AuthServiceTest {
     AuthService authService;
 
     @Mock
-    CustomLogger customLogger;
+    UserDetailsService userDetailsService;
+
+    @Mock
+    PasswordEncoder passwordEncoder;
+
+    @Mock
+    JwtTokenUtil jwtTokenUtil;
 
     @BeforeEach
     public void setup() {
@@ -30,5 +41,35 @@ class AuthServiceTest {
         Boolean result = authService.checkAuth(token, permission);
 
         assertTrue(result);
+    }
+
+    @Test
+    void loginShouldReturnAuthResponseWhenCredentialsMatch() {
+        String login = "validLogin";
+        String password = "validPassword";
+        User user = new User();
+        user.password(passwordEncoder.encode(password));
+
+        when(userDetailsService.loadUserByUsername(login)).thenReturn(user);
+        when(passwordEncoder.matches(password, user.getPassword())).thenReturn(true);
+        when(jwtTokenUtil.generateAccessToken(user)).thenReturn("validToken");
+
+        AuthResponse actualResponse = authService.login(login, password);
+
+        assertEquals(user.id(), actualResponse.id());
+        assertEquals("validToken", actualResponse.accessToken());
+    }
+
+    @Test
+    void loginShouldThrowSecurityExceptionWhenCredentialsDoNotMatch() {
+        String login = "validLogin";
+        String password = "validPassword";
+        User user = new User();
+        user.password(passwordEncoder.encode("wrongPassword"));
+
+        when(userDetailsService.loadUserByUsername(login)).thenReturn(user);
+        when(passwordEncoder.matches(password, user.getPassword())).thenReturn(false);
+
+        assertThrows(SecurityException.class, () -> authService.login(login, password));
     }
 }
