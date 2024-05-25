fd0968207646d36d26c3340907809e67e4ba2f85
pichouch <charles.pichou@reseau.eseo.fr>
2024-05-25 12:40:27 +0200

    FEAT pipeline DB logs

--- backend/src/main/java/fr/eseo/tauri/TauriApplication.java
+++ backend/src/main/java/fr/eseo/tauri/TauriApplication.java
@@ -2,7 +2,9 @@ package fr.eseo.tauri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import me.paulschwarz.springdotenv.DotenvPropertySource;
 
 
 @SpringBootApplication
@@ -11,11 +13,9 @@ public class TauriApplication {
 
 	public static void main(String[] args) {
 		// Run the application
-		String username = System.getenv("DATABASE_USERNAME");
-		String password = System.getenv("DATABASE_PASSWORD");
-		System.out.println("Database Username: " + username);
-		System.out.println("Database Password: " + password);
-		SpringApplication.run(TauriApplication.class, args);
+		SpringApplication app = new SpringApplication(TauriApplication.class);
+		ConfigurableEnvironment env = app.run(args).getEnvironment();
+		DotenvPropertySource.addToEnvironment(env);
 	}
 }
 
