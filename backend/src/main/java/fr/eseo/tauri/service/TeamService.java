678eec6bad40ca51cb2f6f957be29724ac7c8a7b
Théo Marques <theo.marques@reseau.eseo.fr>
2024-05-29 09:38:40 +0200

    fix: permissions on ratingPage, limitation on teams grade with bonuses to 20

--- backend/src/main/java/fr/eseo/tauri/service/TeamService.java
+++ backend/src/main/java/fr/eseo/tauri/service/TeamService.java
@@ -310,7 +310,7 @@ public class TeamService {
 
         for(int i = 0; i < students.size(); i++){
             List<Bonus> studentBonuses = studentService.getStudentBonuses(token, students.get(i).id(), sprintId);
-            double result = 0.7*(teamGrade + studentBonuses.stream().mapToDouble(Bonus::value).sum()) + 0.3*(getIndividualTotalGrades(token, id, sprintId)).get(i);
+            double result = 0.7*(Math.min(teamGrade + studentBonuses.stream().mapToDouble(Bonus::value).sum(), 20.0)) + 0.3*(getIndividualTotalGrades(token, id, sprintId)).get(i);
             sprintGrades.add(formattedResult(result));
         }
         if (sprintGrades.isEmpty()) {
--- frontend/src/components/organisms/Rate/Rating.vue
+++ frontend/src/components/organisms/Rate/Rating.vue
@@ -28,17 +28,25 @@ const props = defineProps<{
 	sprintId: string
 }>()
 
-// const currentUserId = Cookies.getUserId()
-// const { data: currentUserTeam } = useQuery({ queryKey: ["team", currentUserId], queryFn: async() => getTeamByUserId(currentUserId) })
+const currentUserId = Cookies.getUserId()
+const { data: currentUserTeam } = useQuery({ queryKey: ["team", currentUserId], queryFn: async() => getTeamByUserId(currentUserId) })
 
 const { data: allGrades, isLoading, error } = useQuery({ queryKey: ["all-rated-grades"], queryFn: getAllRatedGradesFromConnectedUser })
 
-
+const canGradeGlobalPerformance = hasPermission("GRADE_GLOBAL_PERFORMANCE")
+const canCommentGlobalPerformance = hasPermission("COMMENT_GLOBAL_PERFORMANCE")
 const canGradeLimitedBonus = hasPermission("LIMITED_BONUS_MALUS")
 const canGradeUnlimitedBonus = hasPermission("GIVE_UNLIMITED_BONUS_MALUS")
+const canGradePresentationContent = hasPermission("GRADE_PRESENTATION_CONTENT")
+const canCommentPresentationContent = hasPermission("COMMENT_PRESENTATION_CONTENT")
 const canGradeIndividualPerformance = hasPermission("GRADE_INDIVIDUAL_PERFORMANCE")
 const canCommentIndividualPerformance = hasPermission("COMMENT_INDIVIDUAL_PERFORMANCE")
 const canGradeTechnicalSolution = hasPermission("GRADE_TECHNICAL_SOLUTION")
+const canGradeSprintConformity = hasPermission("GRADE_SPRINT_CONFORMITY")
+const canGradeProjectManagement = hasPermission("GRADE_PROJECT_MANAGEMENT")
+const canCommentTechnicalSolution = hasPermission("COMMENT_TECHNICAL_SOLUTION")
+const canCommentSprintConformity = hasPermission("COMMENT_SPRINT_CONFORMITY")
+const canCommentProjectManagement = hasPermission("COMMENT_PROJECT_MANAGEMENT")
 const canSeeFeedbacks = hasPermission("VIEW_FEEDBACK") && hasPermission("ADD_ALL_TEAMS_FEEDBACK")
 const canSeePrivateComments = hasPermission("ADD_ALL_TEAMS_COMMENT") && hasPermission("VIEW_COMMENT")
 
@@ -49,30 +57,30 @@ const canSeePrivateComments = hasPermission("ADD_ALL_TEAMS_COMMENT") && hasPermi
 
 	<Column v-else class="items-stretch justify-start gap-4">
 		<Row class="w-full items-stretch justify-stretch gap-4">
-			<RatingBox v-if="hasPermission('GRADE_GLOBAL_PERFORMANCE')" class="flex-1"
-				:all-grades="allGrades" grade-type-name="Performance globale de l'équipe" :sprint-id="props.sprintId" :team-id="props.teamId">
+			<RatingBox v-if="(canGradeGlobalPerformance || canCommentGlobalPerformance) && currentUserTeam && currentUserTeam.id.toString() !== props.teamId" class="flex-1"
+				:all-grades="allGrades" grade-type-name="Performance globale de l'équipe" :sprint-id="props.sprintId" :team-id="props.teamId" :grade-authorization="canGradeGlobalPerformance" :comment-authorization="canCommentGlobalPerformance">
 				<Users class="size-6 stroke-[1.33] text-dark-blue" />
 			</RatingBox>
 
-			<RatingBox v-if="hasPermission('GRADE_SPRINT_CONFORMITY')" class="flex-1"
-				:all-grades="allGrades" grade-type-name="Conformité au sprint" :sprint-id="props.sprintId" :team-id="props.teamId">
+			<RatingBox v-if="(canGradeSprintConformity && currentUserTeam && currentUserTeam.id.toString() === props.teamId) || canCommentSprintConformity" class="flex-1"
+				:all-grades="allGrades" grade-type-name="Conformité au sprint" :sprint-id="props.sprintId" :team-id="props.teamId" :grade-authorization="canGradeSprintConformity" :comment-authorization="canCommentSprintConformity">
 				<Play class="size-6 stroke-[1.33] text-dark-blue" />
 			</RatingBox>
 		</Row>
 
 		<Row class="w-full items-stretch justify-stretch gap-4">
-			<RatingBox v-if="hasPermission('GRADE_PRESENTATION_CONTENT')" class="flex-1"
-				:all-grades="allGrades" grade-type-name="Contenu de la présentation" :sprint-id="props.sprintId" :team-id="props.teamId">
+			<RatingBox v-if="canGradePresentationContent || canCommentPresentationContent" class="flex-1"
+				:all-grades="allGrades" grade-type-name="Contenu de la présentation" :sprint-id="props.sprintId" :team-id="props.teamId" :grade-authorization="canGradePresentationContent" :comment-authorization="canCommentPresentationContent">
 				<Presentation class="size-6 stroke-[1.33] text-dark-blue" />
 			</RatingBox>
 
-			<RatingBox v-if="hasPermission('GRADE_PROJECT_MANAGEMENT')" class="flex-1"
-				:all-grades="allGrades" grade-type-name="Gestion de projet" :sprint-id="props.sprintId" :team-id="props.teamId">
+			<RatingBox v-if="(canGradeProjectManagement && currentUserTeam && currentUserTeam.id.toString() === props.teamId) || canCommentProjectManagement" class="flex-1"
+				:all-grades="allGrades" grade-type-name="Gestion de projet" :sprint-id="props.sprintId" :team-id="props.teamId" :grade-authorization="canGradeProjectManagement" :comment-authorization="canCommentProjectManagement">
 				<SquareGanttChart class="size-6 stroke-[1.33] text-dark-blue" />
 			</RatingBox>
 
-			<RatingBox v-if="hasPermission('GRADE_TECHNICAL_SOLUTION')" class="flex-1"
-				:all-grades="allGrades" grade-type-name="Solution Technique" :sprint-id="props.sprintId" :team-id="props.teamId">
+			<RatingBox v-if="(canGradeTechnicalSolution && currentUserTeam && currentUserTeam.id.toString() === props.teamId) || canCommentTechnicalSolution" class="flex-1"
+				:all-grades="allGrades" grade-type-name="Solution Technique" :sprint-id="props.sprintId" :team-id="props.teamId" :grade-authorization="canGradeTechnicalSolution" :comment-authorization="canCommentTechnicalSolution">
 				<Blocks class="size-6 stroke-[1.33] text-dark-blue" />
 			</RatingBox>
 		</Row>
