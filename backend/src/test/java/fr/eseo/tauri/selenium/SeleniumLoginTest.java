2aa61c614cca36ad0bff385f4ecb46b0bde5f57f
pichouch <charles.pichou@reseau.eseo.fr>
2024-06-02 18:53:15 +0200

    CI Gitlab selenium test

--- backend/src/test/java/fr/eseo/tauri/selenium/SeleniumLoginTest.java
+++ backend/src/test/java/fr/eseo/tauri/selenium/SeleniumLoginTest.java
@@ -31,7 +31,10 @@ public class SeleniumLoginTest {
                 ChromeOptions options = new ChromeOptions();
                 options.addArguments("--no-sandbox");
                 options.addArguments("--headless");
-
+                options.addArguments("--ignore-certificate-errors");
+                options.setCapability("goog:loggingPrefs", new LoggingPreferences() {{
+                        enable(LogType.DRIVER, Level.ALL);
+                }});
                 SeleniumLoginTest.webdriver = new ChromeDriver(options);
                 wait = new WebDriverWait(SeleniumLoginTest.webdriver, Duration.ofSeconds(10));
                 SeleniumLoginTest.webdriver.get(SeleniumLoginTest.URL+"login");
