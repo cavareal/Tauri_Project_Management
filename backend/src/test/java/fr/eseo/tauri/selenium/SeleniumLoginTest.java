cc0a9277b45877bf97bbacf5d1a3ba838bbfdc81
pichouch <charles.pichou@reseau.eseo.fr>
2024-06-02 20:55:41 +0200

    CI Gitlab selenium test. Build.gradle without SystemProperties. try http & https

--- backend/src/test/java/fr/eseo/tauri/selenium/SeleniumLoginTest.java
+++ backend/src/test/java/fr/eseo/tauri/selenium/SeleniumLoginTest.java
@@ -17,7 +17,7 @@ import java.time.Duration;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SeleniumLoginTest {
-        private static final String URL = System.getProperty("selenium.server.url", "http://172.24.1.22/");
+        private static final String URL = System.getProperty("selenium.server.url", "https://172.24.1.22/");
         private static final String TITLE = "Bienvenue sur Tauri !";
         private static WebDriver webdriver;
         private static WebDriverWait wait;
