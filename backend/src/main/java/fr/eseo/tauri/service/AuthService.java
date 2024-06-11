4abde40c1b4166b83c04b364065d42a71fa1f276
pichouch <charles.pichou@reseau.eseo.fr>
2024-06-11 08:58:22 +0200

    FIX remove commentaire authService

--- backend/src/main/java/fr/eseo/tauri/service/AuthService.java
+++ backend/src/main/java/fr/eseo/tauri/service/AuthService.java
@@ -35,24 +35,7 @@ public class AuthService {
 
     @Value("${app.log.with.ldap}")
     private String prodProperty;
-
-
-    /*public String getNameFromEmail(String email) {
-        int indexOfDot = email.indexOf(".");
-        int indexOfAt = email.indexOf("@");
-        String name = "";
-
-        if (indexOfDot!= -1 && indexOfAt!= -1) {
-            String firstName = email.substring(0, indexOfDot).trim();
-            String lastName = email.substring(indexOfDot + 1, indexOfAt).trim();
-
-            firstName = Character.toUpperCase(firstName.charAt(0)) + firstName.substring(1);
-            lastName = Character.toUpperCase(lastName.charAt(0)) + lastName.substring(1);
-
-            name = lastName + " " + firstName;
-        }
-        return name;
-    }*/
+    
 
     public AuthResponse login(String email, String password) {
         try {
