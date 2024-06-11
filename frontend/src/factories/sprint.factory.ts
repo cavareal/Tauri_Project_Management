c36ad3799c55f9094598f4567aaabe74a57fe9b5
Quentin TANGUY <quentin.tanguy@reseau.eseo.fr>
2024-06-11 14:34:07 +0200

    test : test sprint service

--- frontend/src/factories/sprint.factory.ts
+++ frontend/src/factories/sprint.factory.ts
@@ -1,13 +1,13 @@
 import type { Sprint } from "@/types/sprint"
-import type { Project } from "@/types/project"
+import { fakeProject } from "@/factories/project.factory"
 
-export const fakeSprint = (project: Project): Sprint => {
+export const fakeSprint = (): Sprint => {
 	return {
 		id: 1,
 		startDate: new Date(),
 		endDate: new Date(),
 		endType: "NORMAL_SPRINT",
 		sprintOrder: 1,
-		project
+		project: fakeProject()
 	}
 }
